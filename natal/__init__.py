ce516b1f35bda1018bee17f22bfde8d350b00881
Kelvin Ng <hoishing@gmail.com>
2024-11-18 12:43:50 +0800

    add PDF demo; update README; pin python version to 3.12

--- natal/__init__.py
+++ natal/__init__.py
@@ -6,3 +6,4 @@ from natal.chart import Chart
 from natal.data import Data
 from natal.stats import Stats, StatData, DistKind
 from natal.config import Config, ThemeType, HouseSys
+from natal.report import Report
