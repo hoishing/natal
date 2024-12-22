5ddbf37bc14a84291ba1c73448d3b3d13ed2cc82
Kelvin Ng <hoishing@gmail.com>
2024-12-22 12:08:54 +0800

    BREAKING CHANGE: move pdf report functionality to separate package 'natal_report' (#4)

--- natal/__init__.py
+++ natal/__init__.py
@@ -6,4 +6,3 @@ from natal.chart import Chart
 from natal.data import Data
 from natal.stats import Stats, StatData, DistKind
 from natal.config import Config, ThemeType, HouseSys
-from natal.report import Report
